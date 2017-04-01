aa329baf46ecf8711daab1c567757dd4bef52a74
Jonathan Goldman <me@jonnii.com>
2017-03-31 21:24:24 -0400

    get icons wired up

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const Funnel = require('broccoli-funnel');
 module.exports = {
   name: 'ember-cli-lightning-design-system',
 
-   treeForStyles() {
-      let nodeModulesPath = this.app.project.nodeModulesPath;
-      let stylePath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system');
+  treeForStyles() {
+    let nodeModulesPath = this.app.project.nodeModulesPath;
+    let stylePath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system');
     
     return new Funnel(stylePath, {
       destDir: 'ember-cli-lightning-design-system'
     });
   },
+
+  treeForPublic() {
+    let nodeModulesPath = this.app.project.nodeModulesPath;
+    let stylePath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system', 'assets', 'icons');
+    
+    return new Funnel(stylePath, {
+      destDir: 'assets/icons'
+    });
+  }
 };
