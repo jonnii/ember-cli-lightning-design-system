b701e012fef47d8177d43ee52595066f986ace0d
Jonathan Goldman <me@jonnii.com>
2017-03-31 21:10:12 -0400

    first component

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /* eslint-env node */
 'use strict';
 
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-cli-lightning-design-system'
+  name: 'ember-cli-lightning-design-system',
+
+   treeForStyles() {
+      let nodeModulesPath = this.app.project.nodeModulesPath;
+      let stylePath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system');
+    
+    return new Funnel(stylePath, {
+      destDir: 'ember-cli-lightning-design-system'
+    });
+  },
 };
