6404fba58e204bcdab63facb26003cf79b8aea4b
Jonathan Goldman <me@jonnii.com>
2017-04-01 19:12:46 -0400

    installation stuff

---
+++ blueprints/ember-cli-lightning-design-system/index.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  name: 'ember-cli-lightning-design-system',
+  description: 'Configure ember-cli-lightning-design-system',
+
+  normalizeEntityName() { }, // no-op since we're just adding dependencies
+
+  afterInstall() {
+    return this.addAddonsToProject({
+      packages: [
+        { name: 'ember-cli-sass' }
+      ]
+    })
+    .then(() => {
+      // Add npm packages to package.json
+      return this.addPackagesToProject([
+        {name: '@salesforce-ux/design-system'}
+      ]);
+    });
+  }
+};
