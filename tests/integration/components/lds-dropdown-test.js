fdd85216560fc0dd54a81bc3b273d2256dedf663
Jonathan Goldman <me@jonnii.com>
2017-04-07 16:31:39 -0400

    move dropdown to component

--- tests/integration/components/lds-dropdown-test.js
+++ tests/integration/components/lds-dropdown-test.js
@@ -1,19 +1,24 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('lds-menu-heading', 'Integration | Component | lds menu heading', {
+moduleForComponent('lds-dropdown', 'Integration | Component | lds dropdown', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  this.render(hbs`{{lds-menu-heading}}`);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{lds-dropdown}}`);
 
   assert.equal(this.$().text().trim(), '');
 
+  // Template block usage:
   this.render(hbs`
-    {{#lds-menu-heading}}
+    {{#lds-dropdown}}
       template block text
-    {{/lds-menu-heading}}
+    {{/lds-dropdown}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
