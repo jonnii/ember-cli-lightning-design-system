b268e8c84cb0c1af1ef3101718575e2d33df4d69
Jonathan Goldman <me@jonnii.com>
2017-04-10 20:20:38 -0400

    pill error

--- addon/components/slds-pill.js
+++ addon/components/slds-pill.js
@@ -3,7 +3,11 @@ import layout from '../templates/components/slds-pill';
 
 export default Ember.Component.extend({
   layout,
-  tagName: '',
 
-  label: 'Label'
+  tagName: 'span',
+  classNames: ['slds-pill'],
+  classNameBindings: ['isError:slds-has-error'],
+
+  label: 'Label',
+  isError: false
 });
--- tests/integration/components/slds-pill-test.js
+++ tests/integration/components/slds-pill-test.js
@@ -18,3 +18,10 @@ test('it renders with label', function(assert) {
   assert.equal(this.$('button').attr('class').trim(), 'slds-pill__remove slds-button slds-button--icon ember-view');
   assert.equal(this.$('a').text().trim(), 'awesome label');
 });
+
+test('it renders with error', function(assert) {
+  this.render(hbs`{{slds-pill isError=true}}`);
+
+  assert.equal(this.$('.slds-pill').attr('class').trim(), 'slds-pill slds-has-error ember-view');
+  assert.equal(this.$('.slds-icon_container').attr('class'), 'slds-icon_container slds-pill__icon_container slds-m-left--xx-small');
+});
