70747093d99e4b7e2ead768052a27a47ce64a796
Jonathan Goldman <me@jonnii.com>
2017-04-17 23:14:56 -0400

    try non-root url

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    fingerprint: {
-      prepend: 'https://jonnii.github.io/'
-    }
   });
 
   /*
