6b3d2d5df4c71aa8f723ff9a2d0b7c133f1b4fc4
Jonathan Goldman <me@jonnii.com>
2017-04-18 22:40:23 -0400

    media bits

--- addon/components/slds-media/body.js
+++ addon/components/slds-media/body.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 import layout from '../../templates/components/slds-media/body';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+  tagName: ''
 });
--- addon/components/slds-media/figure.js
+++ addon/components/slds-media/figure.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 import layout from '../../templates/components/slds-media/figure';
 
+const typeMap = {
+  'default': '',
+  'reverse': 'slds-media__figure--reverse'
+};
+
 export default Ember.Component.extend({
-  layout
+  layout,
+
+  classNames: ['slds-media__figure'],
+  classNameBindings: ['typeClass'],
+
+  type: 'default',
+
+  typeClass: Ember.computed('type', function() {
+    return typeMap[this.get('type')];
+  })
 });
