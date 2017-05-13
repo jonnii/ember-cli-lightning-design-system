4a88a2355f08772be4c81a5b16d6c94d12b85c5b
Jonathan Goldman <me@jonnii.com>
2017-05-13 09:17:21 -0400

    show binding values

--- addon/components/slds-control-group/checkbox.js
+++ addon/components/slds-control-group/checkbox.js
@@ -4,6 +4,7 @@ import identity from '../../mixins/control-identity';
 
 export default Ember.Component.extend(identity, {
   layout,
+
   classNames: 'slds-form-element__control',
   classNameBindings: ['isError:slds-has-error']
 });
