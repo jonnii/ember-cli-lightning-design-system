b12a55c618368b2af770ed92e50fb6744ab04a3c
Jonathan Goldman <me@jonnii.com>
2017-05-13 09:01:25 -0400

    refactor to mixin

--- addon/components/slds-control.js
+++ addon/components/slds-control.js
@@ -1,19 +1,10 @@
 import Ember from 'ember';
 import layout from '../templates/components/slds-control';
+import identity from '../mixins/control-identity';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(identity, {
   layout,
 
   classNames: 'slds-form-element',
-  classNameBindings: ['isError:slds-has-error'],
-
-  formElementId: Ember.computed('elementId', function() {
-    let elementId = this.get('elementId');
-    return `${elementId}-field`;
-  }),
-
-  errorElementId: Ember.computed('elementId', function() {
-    let elementId = this.get('elementId');
-    return `${elementId}-error`;
-  })
+  classNameBindings: ['isError:slds-has-error']
 });
