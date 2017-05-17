45bac3963e8d357a2ef83037a7e6afa43a481dc7
Jonathan Goldman <me@jonnii.com>
2017-05-16 20:32:19 -0400

    fix waffle

--- tests/integration/components/slds-icon-waffle-test.js
+++ tests/integration/components/slds-icon-waffle-test.js
@@ -9,4 +9,13 @@ test('it renders', function(assert) {
   this.render(hbs`{{slds-icon-waffle}}`);
 
   assert.equal(this.$('.slds-icon-waffle').length, 1);
+  assert.notOk(this.$('button').attr('title'), 'should not have title');
+  assert.notOk(this.$('span.slds-assistive-text').length, 'should not have assistive text');
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`{{slds-icon-waffle title='amazing title'}}`);
+
+  assert.equal(this.$('button').attr('title'), 'amazing title');
+  assert.equal(this.$('span.slds-assistive-text').text().trim(), 'amazing title');
 });
