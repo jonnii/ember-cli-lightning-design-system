37c9f55e7ceef23f272b1ac4f562fb0ed8a8a7a5
Jonathan Goldman <me@jonnii.com>
2017-05-16 21:21:51 -0400

    Checkbox control

--- tests/integration/components/slds-control/checkbox-test.js
+++ tests/integration/components/slds-control/checkbox-test.js
@@ -1,24 +1,24 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('slds-control-group/checkbox', 'Integration | Component | slds control group/checkbox', {
+moduleForComponent('slds-control/checkbox', 'Integration | Component | slds control/checkbox', {
   integration: true
 });
 
 test('it renders with label', function(assert) {
-  this.render(hbs`{{slds-control-group/checkbox label='awesome sauce'}}`);
+  this.render(hbs`{{slds-control/checkbox label='awesome sauce'}}`);
 
   assert.equal(this.$('.slds-form-element__label').text(), 'awesome sauce');
 });
 
 test('it renders with correct label', function(assert) {
-  this.render(hbs`{{slds-control-group/checkbox label='awesome sauce'}}`);
+  this.render(hbs`{{slds-control/checkbox label='awesome sauce'}}`);
 
   assert.equal(this.$('input').attr('id'), this.$('label').attr('for'));
 });
 
 test('it renders disabled', function(assert) {
-  this.render(hbs`{{slds-control-group/checkbox isDisabled=true label='awesome sauce'}}`);
+  this.render(hbs`{{slds-control/checkbox isDisabled=true label='awesome sauce'}}`);
 
   assert.ok(this.$('input:disabled'));
 });
