d038eb6a6ad33bacb4f4134b5878ee346a8d3f39
Jonathan Goldman <me@jonnii.com>
2017-05-18 23:15:19 -0400

    tests

--- tests/integration/components/slds-control-group/radio-button-test.js
+++ tests/integration/components/slds-control-group/radio-button-test.js
@@ -6,20 +6,7 @@ moduleForComponent('slds-control-group/radio-button', 'Integration | Component |
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{slds-control-group/radio-button}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#slds-control-group/radio-button}}
-      template block text
-    {{/slds-control-group/radio-button}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.expect(0);
 });
