964c19d010067a1db381f2a22008a737c425f554
Jonathan Goldman <me@jonnii.com>
2017-05-18 20:57:16 -0400

    [Feature] Progress bar improvements

--- addon/components/slds-progress-bar.js
+++ addon/components/slds-progress-bar.js
@@ -1,14 +1,43 @@
 import Ember from 'ember';
 import layout from '../templates/components/slds-progress-bar';
 
+const heightMap = {
+  default: '',
+  'x-small': 'slds-progress-bar_x-small',
+  small: 'slds-progress-bar_small',
+  medium: 'slds-progress-bar_medium',
+  large: 'slds-progress-bar_large'
+};
+
 export default Ember.Component.extend({
   layout,
-  tagName: '',
+
+  classNames: ['slds-progress-bar'],
+  classNameBindings: ['heightClass', 'borderClass'],
+  attributeBindings: ['role:role', 'minValue:aria-valuemin', 'maxValue:aria-valuemax', 'value:aria-valuenow'],
+
+  role: 'progressbar',
+  minValue: 0,
+  maxValue: 100,
   value: 0,
+  height: 'default',
 
   progressStyle: Ember.computed('value', function() {
     const value = this.get('value');
     const parsed = parseInt(value, 10);
     return Ember.String.htmlSafe(`width:${parsed}%;`);
+  }),
+
+  heightClass: Ember.computed('height', function() {
+    const height = this.get('height');
+    return heightMap[height];
+  }),
+
+  borderClass: Ember.computed('border', function() {
+    const border = this.get('border');
+    if (border == 'circular') {
+      return 'slds-progress-bar_circular';
+    }
+    return '';
   })
 });
