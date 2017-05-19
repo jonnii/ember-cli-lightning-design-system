671dc6baac9f02ffa9d31063d195945dfee92d6c
Jonathan Goldman <me@jonnii.com>
2017-05-18 22:47:43 -0400

    [Feature] Radio buttons

---
+++ addon/components/slds-control-group/radio-button.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../../templates/components/slds-control-group/radio-button';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: ''
+});
---
+++ app/components/slds-control-group/radio-button.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-lightning-design-system/components/slds-control-group/radio-button';
