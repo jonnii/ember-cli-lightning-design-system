87711bf0ab9f6bcc90a7023bc0e746e2a3a31d24
Doucy, Pierre <Pierre.Doucy@mlp.com>
2017-05-19 12:19:59 -0400

    optional static text

--- addon/components/slds-navigation-bar.js
+++ addon/components/slds-navigation-bar.js
@@ -3,5 +3,6 @@ import layout from '../templates/components/slds-navigation-bar';
 
 export default Ember.Component.extend({
   layout,
-  applicationName: 'App Name'
+  applicationName: 'App Name',
+  staticText: null
 });
