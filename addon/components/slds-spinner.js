29fdba1c9a7cc7ae59458f1d88756e89beb7b878
Jonathan Goldman <me@jonnii.com>
2017-05-18 20:02:13 -0400

    color and size

--- addon/components/slds-spinner.js
+++ addon/components/slds-spinner.js
@@ -1,7 +1,37 @@
 import Ember from 'ember';
 import layout from '../templates/components/slds-spinner';
 
+const sizeMap = {
+  'xx-small': 'slds-spinner_xx-small',
+  'x-small': 'slds-spinner_x-small',
+  small: 'slds-spinner_small',
+  medium: 'slds-spinner_medium',
+  large: 'slds-spinner_large'
+};
+
+const colorMap = {
+  default: '',
+  brand: 'slds-spinner_brand',
+  inverse: 'slds-spinner_inverse'
+};
+
 export default Ember.Component.extend({
   layout,
-  tagName: ''
+
+  classNames: ['slds-spinner'],
+  classNameBindings: ['sizeClass', 'colorClass'],
+  attributeBindings: ['role:status'],
+
+  size: 'medium',
+  color: 'default',
+
+  sizeClass: Ember.computed('size', function() {
+    const size = this.get('size');
+    return sizeMap[size];
+  }),
+
+  colorClass: Ember.computed('color', function() {
+    const color = this.get('color');
+    return colorMap[color];
+  })
 });
