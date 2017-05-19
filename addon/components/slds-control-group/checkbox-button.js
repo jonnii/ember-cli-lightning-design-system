6c8b66fbd8f2a1a4bb4c7a93998c550f455ab9d1
Jonathan Goldman <me@jonnii.com>
2017-05-18 23:28:17 -0400

    more

---
+++ addon/components/slds-control-group/checkbox-button.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../../templates/components/slds-control-group/checkbox-button';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: ''
+});
---
+++ tests/integration/components/slds-control-group/checkbox-button-test.js
@@ -0,0 +1,12 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('slds-control-group/checkbox-button', 'Integration | Component | slds control group/checkbox button', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`{{slds-control-group/checkbox-button}}`);
+
+  assert.expect(0);
+});
