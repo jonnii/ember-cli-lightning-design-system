34b4574077ed29f958f0d714f16d713c68f540f3
Jonathan Goldman <me@jonnii.com>
2017-05-22 19:46:30 -0400

    [Feature] Alerts

--- addon/components/slds-alert.js
+++ addon/components/slds-alert.js
@@ -1,6 +1,25 @@
 import Ember from 'ember';
 import layout from '../templates/components/slds-alert';
 
+const themeMap = {
+  info: 'slds-theme_info',
+  error: 'slds-theme_error',
+  warning: 'slds-theme_warning',
+  offline: 'slds-theme_offline'
+};
+
 export default Ember.Component.extend({
-  layout
+  layout,
+
+  classNames: ['slds-notify', 'slds-notify--alert', 'slds-theme--alert-texture'],
+  classNameBindings: ['themeClass'],
+  attributeBindings: ['role:role'],
+
+  role: 'alert',
+  theme: 'info',
+
+  themeClass: Ember.computed('theme', function() {
+    const theme = this.get('theme');
+    return themeMap[theme];
+  })
 });
