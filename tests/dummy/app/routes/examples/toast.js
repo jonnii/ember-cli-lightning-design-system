5beae19b0fac2639b4c63929f982f36d3399a9ea
Jonathan Goldman <me@jonnii.com>
2017-05-22 14:55:36 -0400

    wire up template

--- tests/dummy/app/routes/examples/toast.js
+++ tests/dummy/app/routes/examples/toast.js
@@ -4,8 +4,20 @@ export default Ember.Route.extend({
   flashMessages: Ember.inject.service(),
 
   actions: {
-    showToast() {
-      Ember.get(this, 'flashMessages').success('Success!');
+    showInfo() {
+      Ember.get(this, 'flashMessages').info('Amazing story bro!');
+    },
+
+    showSuccess() {
+      Ember.get(this, 'flashMessages').success('Amazing story bro!');
+    },
+
+    showWarning() {
+      Ember.get(this, 'flashMessages').warning('Amazing story bro!');
+    },
+
+    showError() {
+      Ember.get(this, 'flashMessages').danger('Amazing story bro!');
     }
   }
 });
