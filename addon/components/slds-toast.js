1bf66133b995beb0e6bc5a973dcc07bf6fea95b6
Jonathan Goldman <me@jonnii.com>
2017-05-22 15:13:05 -0400

    more theme

--- addon/components/slds-toast.js
+++ addon/components/slds-toast.js
@@ -1,11 +1,23 @@
 import Ember from 'ember';
 import layout from '../templates/components/slds-toast';
 
+const themeMap = {
+  info: 'slds-theme_info',
+  success: 'slds-theme_success'
+};
+
 export default Ember.Component.extend({
   layout,
 
-  classNames: ['slds-notify', 'slds-notify_toast', 'slds-theme_info'],
+  classNames: ['slds-notify', 'slds-notify_toast'],
+  classNameBindings: ['themeClass'],
   attributeBindings: ['role:role'],
 
-  role: 'alert'
+  role: 'alert',
+  theme: 'info',
+
+  themeClass: Ember.computed('theme', function() {
+    const theme = this.get('theme');
+    return themeMap[theme];
+  })
 });
