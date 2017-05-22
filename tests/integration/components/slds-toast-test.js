401879bd5ea4214510f9fa1b53f41b0c06ee83b1
Jonathan Goldman <me@jonnii.com>
2017-05-22 15:13:05 -0400

    more theme

--- tests/integration/components/slds-toast-test.js
+++ tests/integration/components/slds-toast-test.js
@@ -9,4 +9,13 @@ test('it renders', function(assert) {
   this.render(hbs`{{slds-toast}}`);
 
   assert.equal(this.$('.slds-notify').attr('role'), 'alert');
+  assert.ok(this.$('.slds-theme_info').length);
+  assert.equal(this.$('.slds-notify > .slds-assistive-text').text().trim(), 'info');
+});
+
+test('it renders success theme', function(assert) {
+  this.render(hbs`{{slds-toast theme='success'}}`);
+
+  assert.ok(this.$('.slds-theme_success').length);
+  assert.equal(this.$('.slds-notify > .slds-assistive-text').text().trim(), 'success');
 });
