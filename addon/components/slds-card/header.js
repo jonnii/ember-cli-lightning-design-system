61900ec860e31faddd2b865e9ac1b0384489529d
Jonathan Goldman <me@jonnii.com>
2017-09-13 18:18:05 -0400

    demo layout

---
+++ addon/components/slds-card/header.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../../templates/components/slds-card/header';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['slds-card__header']
+});
