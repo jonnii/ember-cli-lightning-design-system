83ce8293ab0eba63850367f55d18f27ed910de5c
jonnii <me@jonnii.com>
2017-09-19 11:28:06 -0400

    clean up

--- tests/integration/components/slds-path-test.js
+++ tests/integration/components/slds-path-test.js
@@ -6,29 +6,7 @@ moduleForComponent('slds-path', 'Integration | Component | slds path', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{slds-path}}`);
 
-  assert.equal(
-    this.$()
-      .text()
-      .trim(),
-    ''
-  );
-
-  // Template block usage:
-  this.render(hbs`
-    {{#slds-path}}
-      template block text
-    {{/slds-path}}
-  `);
-
-  assert.equal(
-    this.$()
-      .text()
-      .trim(),
-    'template block text'
-  );
+  assert.ok(this.$('.slds-path').length);
 });
