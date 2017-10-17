0f8ffb81ed6e31c7b3660de4d8c663f39799f84d
jonnii <me@jonnii.com>
2017-10-17 13:29:22 -0400

    wrap default input

--- tests/integration/components/slds-control/input-test.js
+++ tests/integration/components/slds-control/input-test.js
@@ -7,21 +7,21 @@ moduleForComponent('slds-control/input', 'Integration | Component | slds control
 
 test('it renders', function(assert) {
   this.render(hbs`{{slds-control/input}}`);
-  assert.ok(this.$('.slds-form-element__control input'));
+  assert.ok(this.$('.slds-form-element__control input').length);
 });
 
 test('it renders with icon (left)', function(assert) {
   this.render(hbs`{{slds-control/input icon='left'}}`);
-  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon.slds-input-has-icon_left input'));
+  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon.slds-input-has-icon_left input').length);
 });
 
 test('it renders with icon (right)', function(assert) {
   this.render(hbs`
     {{#slds-control/input icon='right' as |input|}}
-      {{input.icon}}
+      {{input.icon-left}}
     {{/slds-control/input}}
   `);
 
-  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon.slds-input-has-icon_right input'));
-  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon slds-input__icon.slds-input__icon--left'));
+  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon.slds-input-has-icon_right input').length);
+  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon .slds-input__icon.slds-input__icon--left').length);
 });
