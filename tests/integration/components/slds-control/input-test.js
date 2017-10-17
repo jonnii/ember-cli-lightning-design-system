60a04c137960747325a40a1cf236d9dda70e9620
Jonathan Goldman <me@jonnii.com>
2017-10-17 04:09:17 -0400

    icon bindings

--- tests/integration/components/slds-control/input-test.js
+++ tests/integration/components/slds-control/input-test.js
@@ -7,6 +7,15 @@ moduleForComponent('slds-control/input', 'Integration | Component | slds control
 
 test('it renders', function(assert) {
   this.render(hbs`{{slds-control/input}}`);
+  assert.ok(this.$('.slds-form-element__control input'));
+});
 
-  assert.expect(0);
+test('it renders with icon (left)', function(assert) {
+  this.render(hbs`{{slds-control/input icon='left'}}`);
+  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon.slds-input-has-icon_left input'));
+});
+
+test('it renders with icon (right)', function(assert) {
+  this.render(hbs`{{slds-control/input icon='right'}}`);
+  assert.ok(this.$('.slds-form-element__control.slds-input-has-icon.slds-input-has-icon_right input'));
 });
