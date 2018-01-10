2ca0d0a64d61700732d137b93720584740dd1c3e
Jonathan Goldman <me@jonnii.com>
2018-01-09 22:45:27 -0500

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,14 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  plugins: ["ember", "prettier"],
-  extends: ["eslint:recommended", "plugin:ember/recommended"],
+  plugins: ['ember', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
     browser: true
   },
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         printWidth: 120,
         singleQuote: true
@@ -21,36 +21,26 @@ module.exports = {
   overrides: [
     // node files
     {
-      files: [
-        "index.js",
-        "testem.js",
-        "ember-cli-build.js",
-        "config/**/*.js",
-        "tests/dummy/config/**/*.js"
-      ],
-      excludedFiles: ["app/**", "addon/**"],
+      files: ['index.js', 'testem.js', 'ember-cli-build.js', 'config/**/*.js', 'tests/dummy/config/**/*.js'],
+      excludedFiles: ['app/**', 'addon/**'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
         ecmaVersion: 2015
       },
       env: {
         browser: false,
         node: true
       },
-      plugins: ["node"],
-      rules: Object.assign(
-        {},
-        require("eslint-plugin-node").configs.recommended.rules,
-        {
-          // add your custom rules and overrides for node files here
-        }
-      )
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
     },
 
     // test files
     {
-      files: ["tests/**/*.js"],
-      excludedFiles: ["tests/dummy/**/*.js"],
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
       env: {
         embertest: true
       }
