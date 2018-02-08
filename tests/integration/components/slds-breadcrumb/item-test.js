20d8bae81fd6538c309fb98e457c90f91e46d5d1
Jonathan Goldman <me@jonnii.com>
2018-02-07 23:40:09 -0500

    Better breadcrumbs (#37)

--- tests/integration/components/slds-breadcrumb/item-test.js
+++ tests/integration/components/slds-breadcrumb/item-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('slds-breadcrumb-item', 'Integration | Component | slds breadcrumb item', {
+moduleForComponent('slds-breadcrumb/item', 'Integration | Component | slds breadcrumb/item', {
   integration: true
 });
 
@@ -9,7 +9,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{slds-breadcrumb-item}}`);
+  this.render(hbs`{{slds-breadcrumb/item}}`);
 
   assert.equal(
     this.$()
@@ -20,9 +20,9 @@ test('it renders', function(assert) {
 
   // Template block usage:
   this.render(hbs`
-    {{#slds-breadcrumb-item}}
+    {{#slds-breadcrumb/item}}
       template block text
-    {{/slds-breadcrumb-item}}
+    {{/slds-breadcrumb/item}}
   `);
 
   assert.equal(
