b36c8bb92446ef9c8247662f4bdb2f22b1d2a544
Jonathan Goldman <me@jonnii.com>
2018-02-07 23:25:40 -0500

    Better breadcrumbs

--- addon/components/slds-breadcrumb/item.js
+++ addon/components/slds-breadcrumb/item.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import layout from '../templates/components/slds-breadcrumb-item';
+import layout from '../../templates/components/slds-breadcrumb/item';
 
 export default Component.extend({
   layout,
