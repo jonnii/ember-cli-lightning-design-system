c31eac846f3425bc9de2330a2838841dcc6a3952
Jonathan Goldman <me@jonnii.com>
2018-02-10 19:29:13 -0500

    More card related bits (#43)
    * More card related bits
    
    * use media component

--- tests/integration/components/slds-card-test.js
+++ tests/integration/components/slds-card-test.js
@@ -6,8 +6,22 @@ moduleForComponent('slds-card', 'Integration | Component | slds card', {
 });
 
 test('it renders', function(assert) {
-  this.render(hbs`{{slds-card}}`);
+  this.render(hbs`
+    {{#slds-card as |card|}}
+      {{#card.header}}
+        header
+      {{/card.header}}
+      {{#card.body}}
+        body
+      {{/card.body}}
+      {{#card.footer}}
+        footer
+      {{/card.footer}}
+    {{/slds-card}}
+  `);
 
-  assert.ok(this.$('header.slds-media.slds-media--center').length);
   assert.ok(this.$('article.slds-card').length);
+  assert.ok(this.$('article.slds-card .slds-card__header').length, 'has header');
+  assert.ok(this.$('article.slds-card .slds-card__body.slds-card__body_inner').length, 'has body');
+  assert.ok(this.$('article.slds-card footer.slds-card__footer').length, 'has footer');
 });
