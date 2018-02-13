67973eaafacb60ef8bd7158eb8f28f543d635ffc
Jonathan Goldman <me@jonnii.com>
2018-02-12 20:50:42 -0500

    Refactor box and add size

--- addon/components/slds-box.js
+++ addon/components/slds-box.js
@@ -1,7 +1,21 @@
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 import layout from '../templates/components/slds-box';
 
+const sizeMap = {
+  default: '',
+  small: 'slds-box_small',
+  'x-small': 'slds-box_x-small',
+  'xx-small': 'slds-box_xx-small'
+};
+
 export default Component.extend({
   layout,
-  tagName: ''
+  classNames: 'slds-box',
+  classNameBindings: ['sizeClass'],
+
+  size: 'default',
+  sizeClass: computed('size', function() {
+    return sizeMap[this.get('size')];
+  })
 });
