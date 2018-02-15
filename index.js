6445958f450858a07772b7f2829c91acee6322ef
Jonathan Goldman <me@jonnii.com>
2018-02-14 22:11:03 -0500

    Ember 3.0

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ module.exports = {
   name: 'ember-cli-lightning-design-system',
 
   treeForStyles() {
-    let nodeModulesPath = this.app.project.nodeModulesPath;
+    let root = this.app.project.root;
 
-    let stylePath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system', 'scss');
+    let stylePath = path.join(root, 'node_modules', '@salesforce-ux', 'design-system', 'scss');
     let styles = new Funnel(stylePath, {
       destDir: 'ember-cli-lightning-design-system/scss'
     });
 
-    let designTokensPath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system', 'design-tokens', 'dist');
+    let designTokensPath = path.join(root, 'node_modules', '@salesforce-ux', 'design-system', 'design-tokens', 'dist');
     let designTokens = new Funnel(designTokensPath, {
       destDir: 'ember-cli-lightning-design-system/design-tokens/dist'
     });
@@ -24,19 +24,19 @@ module.exports = {
   },
 
   treeForPublic() {
-    let nodeModulesPath = this.app.project.nodeModulesPath;
+    let root = this.app.project.root;
 
-    let iconsPath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system', 'assets', 'icons');
+    let iconsPath = path.join(root, 'node_modules', '@salesforce-ux', 'design-system', 'assets', 'icons');
     let icons = new Funnel(iconsPath, {
       destDir: 'assets/icons'
     });
 
-    let fontsPath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system', 'assets', 'fonts');
+    let fontsPath = path.join(root, 'node_modules', '@salesforce-ux', 'design-system', 'assets', 'fonts');
     let fonts = new Funnel(fontsPath, {
       destDir: 'fonts'
     });
 
-    let imagesPath = path.join(nodeModulesPath, '@salesforce-ux', 'design-system', 'assets', 'images');
+    let imagesPath = path.join(root, 'node_modules', '@salesforce-ux', 'design-system', 'assets', 'images');
     let images = new Funnel(imagesPath, {
       destDir: 'assets/images'
     });
