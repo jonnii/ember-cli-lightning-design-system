d7b6ef9243a5ba7ae1a350ad9ee22ffd8b0c3a02
Jonathan Goldman <me@jonnii.com>
2018-04-04 23:20:18 -0400

    fix a thing

--- tests/integration/components/slds-modal-container-test.js
+++ tests/integration/components/slds-modal-container-test.js
@@ -1,4 +1,4 @@
-import { find } from 'ember-native-dom-helpers';
+import { findAll } from 'ember-native-dom-helpers';
 
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
