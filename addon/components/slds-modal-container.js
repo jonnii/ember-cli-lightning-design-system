1744079469572fc6a07d0f4e9ba1625927e7a0c5
Jonathan Goldman <me@jonnii.com>
2018-04-04 23:18:22 -0400

    clean up

--- addon/components/slds-modal-container.js
+++ addon/components/slds-modal-container.js
@@ -4,11 +4,12 @@ import layout from '../templates/components/slds-modal-container';
 
 export default Component.extend({
   layout,
+
   classNames: 'slds-modal',
   classNameBindings: 'isShowing:slds-fade-in-open',
   attributeBindings: ['role:role'],
-  role: 'dialog',
 
+  role: 'dialog',
   isShowing: false,
 
   didInsertElement() {
--- tests/integration/components/slds-app-launcher-test.js
+++ tests/integration/components/slds-app-launcher-test.js
@@ -7,20 +7,7 @@ module('Integration | Component | slds-app-launcher', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{slds-app-launcher}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#slds-app-launcher}}
-        template block text
-      {{/slds-app-launcher}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.expect(0);
   });
 });
