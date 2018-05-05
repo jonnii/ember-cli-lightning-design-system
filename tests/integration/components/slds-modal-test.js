7d0e0f2340f8d572c39824f1bb0d096d55251668
Jonathan Goldman <me@jonnii.com>
2018-05-04 22:28:03 -0400

    modal stuff

--- tests/integration/components/slds-modal-test.js
+++ tests/integration/components/slds-modal-test.js
@@ -2,12 +2,13 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { findAll } from 'ember-native-dom-helpers';
 
 module('Integration | Component | slds modal', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     await render(hbs`{{slds-modal}}`);
-    assert.expect(0);
+    assert.equal(findAll('section[role=dialog]').length, 1);
   });
 });
