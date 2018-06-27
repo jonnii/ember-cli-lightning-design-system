479f9444bcce03c69b3a024ba3155de6bff9b0fe
jonnii <me@jonnii.com>
2018-06-27 15:25:51 -0400

    [Feature] Sections

---
+++ addon/components/slds-section.js
@@ -0,0 +1,20 @@
+import { guidFor } from '@ember/object/internals';
+import { computed } from '@ember/object';
+import Component from '@ember/component';
+import layout from '../templates/components/slds-section';
+
+export default Component.extend({
+  layout,
+  classNames: ['slds-section'],
+  classNameBindings: ['isOpen:slds-is-open', 'isCollapsable::slds-is-open'],
+
+  isCollapsable: true,
+
+  sectionId: computed(function() {
+    return guidFor(this);
+  }),
+
+  isHidden: computed('isOpen', function() {
+    return !this.isOpen;
+  })
+});
